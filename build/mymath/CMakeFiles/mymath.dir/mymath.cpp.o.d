4dbb219870af9340357328fe84086e3591fd0d24
GrifkaTop <grifkatop@gmail.com>
2025-02-19 15:14:41 +0300

    1

---
+++ build/mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
@@ -0,0 +1,4 @@
+mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
+ /home/grifka/2025-1\ BMSTU\ labs/2025-1-BMSTU-labs/mymath/mymath.cpp \
+ /usr/include/stdc-predef.h \
+ /home/grifka/2025-1\ BMSTU\ labs/2025-1-BMSTU-labs/mymath/mymath.h
